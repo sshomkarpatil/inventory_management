8504dca240d7ede8a69a33087ac096466962667e
Omkar Patil <omkarpatil2510@gmail.com>
2019-07-16 20:13:42 +0530

    Add seed file to create warehouses and products

--- app/models/warehouses_product.rb
+++ app/models/warehouses_product.rb
@@ -1,4 +1,5 @@
 class WarehousesProduct < ApplicationRecord
+
   DEFAULT_ITEM_COUNT = 0
   DEFAULT_LOW_ITEM_THRESHOLD = 10
 
@@ -13,6 +14,8 @@ class WarehousesProduct < ApplicationRecord
   validates_uniqueness_of :product, scope: :warehouse
   validates_uniqueness_of :warehouse, scope: :product
 
+  before_validation :set_default_item_count, :set_default_low_item_threshold
+
   def self.default_import_hash(product, warehouse)
     {
       product_id: product.id,
@@ -20,5 +23,13 @@ class WarehousesProduct < ApplicationRecord
       item_count: DEFAULT_ITEM_COUNT,
       low_item_threshold: DEFAULT_LOW_ITEM_THRESHOLD
     }
+  end
+
+  def set_default_item_count
+    self.item_count = DEFAULT_ITEM_COUNT if self.item_count.blank?
+  end
+
+  def set_default_low_item_threshold
+    self.low_item_threshold = DEFAULT_LOW_ITEM_THRESHOLD if self.low_item_threshold.blank?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-(1..60).each do |i|
-  sku = 10000000 + i
-  price = (rand * 1000).round(2)
-  product = Product.new(name: "Sample Product #{i}", sku: sku, price: price)
-  product.save(validate: false)
+Product.without_callback(:create, :after, :create_warehouses_products) do
+  (1..60).each { |i| product = Product.create!(name: "Sample Product #{i}", sku: 10000000 + i, price: (rand * 1000).round(2)) }
 end
 
 warehouses = ([
@@ -19,6 +16,39 @@ warehouses = ([
   { name: 'Bangalore', wh_code: 'Ban200045', pincode: '200045', max_capacity: 10_000 }
 ])
 
-warehouses.each do |warehouse|
-  Warehouse.new(warehouse).save
+Warehouse.without_callback(:create, :after, :create_warehouses_products) do
+  warehouses.each { |warehouse| Warehouse.create!(warehouse) }
+end
+
+# Mumbai Warehouse Products
+mumbai_warehouse = Warehouse.find_by(wh_code: 'Mum400037')
+Product.all.each do |product|
+  item_count = rand(11..50)
+  mumbai_warehouse.warehouses_products.create!(product: product, item_count: item_count)
+end
+
+# Banglore Warehouse Products
+bangalore_warehouse = Warehouse.find_by(wh_code: 'Ban200045')
+low_threashold_product = Product.count / 3
+Product.all.limit(low_threashold_product).each do |product|
+  item_count = rand(1..9)
+  bangalore_warehouse.warehouses_products.create!(product: product, item_count: item_count)
+end
+p_ids = bangalore_warehouse.products.pluck(:id)
+rest_products = Product.where.not(id: p_ids)
+rest_products.each do |product|
+  item_count = rand(11..50)
+  bangalore_warehouse.warehouses_products.create!(product: product, item_count: item_count)
+end
+
+# New Delhi Warehouse Products
+new_delhi_warehouse = Warehouse.find_by(wh_code: 'ND100036')
+groups = Product.all.in_groups(2)
+groups[0].each do |product|
+  item_count = rand(1..9)
+  new_delhi_warehouse.warehouses_products.create!(product: product, item_count: item_count)
+end
+groups[1].each do |product|
+  item_count= rand(11..50)
+  new_delhi_warehouse.warehouses_products.create!(product: product, item_count: item_count)
 end
